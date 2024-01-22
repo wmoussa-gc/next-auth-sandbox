20ed49a92af2a7030f56ef2bd3e49d6c1896f788
Wissam Moussa <wissam.moussa@cds-snc.ca>
2024-01-22 10:48:46 -0500

    another fix

--- terragrunt/aws/ecr.tf
+++ terragrunt/aws/ecr.tf
@@ -1,6 +1,4 @@
 resource "aws_ecr_repository" "web" {
-  # checkov:skip=CKV_AWS_51:The :latest tag is used in Staging
-  # checkov:skip=CKV_AWS_136: ECR encryption with default KMS key is acceptable
 
   name                 = "${var.product_name}/web"
   image_tag_mutability = "MUTABLE"
